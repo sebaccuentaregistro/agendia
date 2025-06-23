0085e69500da61665d5965e74e60db7d316a288b
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-06-23 16:10:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/students/page.tsx
+++ src/app/(main)/students/page.tsx
@@ -272,7 +272,7 @@ export default function StudentsPage() {
                     </DropdownMenu>
                   </TableCell>
                 </TableRow>
-              )
+              );
             })}
           </TableBody>
         </Table>
