7ba088084b99e355afcf7bdf189491bf069451c1
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-06-24 16:14:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/schedule/page.tsx
+++ src/app/(main)/schedule/page.tsx
@@ -5,8 +5,10 @@ import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
+  CardTitle,
 } from '@/components/ui/card';
 import { PlusCircle, Trash2, Pencil, Users } from 'lucide-react';
 import {
