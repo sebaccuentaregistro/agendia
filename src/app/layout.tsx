c2ecb2b968da0aebbd86a6beae147eba32eb1366
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-06-27 19:20:36 +0000

    quisiera que en celular, aparezca una barrita fija abajo que nunca se va

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { StudioProvider } from '@/context/StudioContext';
 import AppHeader from '@/components/layout/app-header';
+import { MobileBottomNav } from '@/components/layout/mobile-bottom-nav';
 
 export const metadata: Metadata = {
   title: 'Agendia',
@@ -21,9 +22,10 @@ export default function RootLayout({
         <StudioProvider>
           <div className="flex min-h-screen w-full flex-col">
             <AppHeader />
-            <main className="flex-grow p-4 sm:p-6 lg:p-8">
+            <main className="flex-grow p-4 sm:p-6 lg:p-8 pb-20 md:pb-8">
               {children}
             </main>
+            <MobileBottomNav />
           </div>
           <Toaster />
         </StudioProvider>
