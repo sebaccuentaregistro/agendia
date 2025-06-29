a42eb378482ce4e89dfc91911cec3c56a0d9ebe0
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-06-29 23:24:11 +0000

    me habia equivocado es esto   // Import the functions you need from the

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,8 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration is now managed in the .env file
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
