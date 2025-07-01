b3035d706f9d7d5ed6306a40e889bc0b8fd9ef34
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-07-01 22:18:18 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,15 +4,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
-// Your web app's Firebase configuration is now managed in the .env file
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAre4fsapbFOgaNmMYWKxkulhMdmMA8Lts",
-  authDomain: "agendia-57247.firebaseapp.com",
-  projectId: "agendia-57247",
-  storageBucket: "agendia-57247.appspot.com",
-  messagingSenderId: "145587178960",
-  appId: "1:145587178960:web:77f0a756d9c6f58c0b7dd5",
-  measurementId: "G-MX0WCMGDLM"
+  apiKey: "AIzaSyDMlnGw4QdX8TmRy-uGQr0iseudb9cy4TM",
+  authDomain: "yogaflow-manager-uqjpc.firebaseapp.com",
+  projectId: "yogaflow-manager-uqjpc",
+  storageBucket: "yogaflow-manager-uqjpc.firebasestorage.app",
+  messagingSenderId: "230355743596",
+  appId: "1:230355743596:web:48c5af60d64ff075d8b949"
 };
 
 
