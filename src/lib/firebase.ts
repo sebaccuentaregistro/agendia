6eff258338569f13f9848123ddb15412d7f497d8
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-07-01 21:54:14 +0000

    vuelve el ultimo cambio que hiciste para atras, la has roto te dije que

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,9 +7,9 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 // Your web app's Firebase configuration is now managed in the .env file
 const firebaseConfig = {
   apiKey: "AIzaSyAre4fsapbFOgaNmMYWKxkulhMdmMA8Lts",
-  authDomain: "yogaflow-manager-uqjpc.firebaseapp.com",
-  projectId: "yogaflow-manager-uqjpc",
-  storageBucket: "yogaflow-manager-uqjpc.appspot.com",
+  authDomain: "agendia-57247.firebaseapp.com",
+  projectId: "agendia-57247",
+  storageBucket: "agendia-57247.appspot.com",
   messagingSenderId: "145587178960",
   appId: "1:145587178960:web:77f0a756d9c6f58c0b7dd5",
   measurementId: "G-MX0WCMGDLM"
