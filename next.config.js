f8f1afcb826ed4dcfdc4e10de077d7c90cbbd560
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-07-03 17:49:40 +0000

    esto arroja en prod: Application error: a client-side exception has occu

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
