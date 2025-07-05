e47862c670e71f0d9c63c36b35a182a3f5268cab
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-07-05 19:23:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 // For local development, you can create a `.env.local` file at the root of your
 // project and add the configuration values there. See the `.env.local.example` file.
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDMlnGw4QdX8TmRy-uGQr0iseudb9cy4TM",
+  authDomain: "yogaflow-manager-uqjpc.firebaseapp.com",
+  projectId: "yogaflow-manager-uqjpc",
+  storageBucket: "yogaflow-manager-uqjpc.appspot.com",
+  messagingSenderId: "230355743596",
+  appId: "1:230355743596:web:91e896c93c7f0965d8b949",
 };
 
 
