0f8b976f78f5a88a4d287c6a573553af49ce3d06
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-07-13 11:40:23 +0000

    se ve horrendo mira la imagen, lastima porque el build esta bien salio o

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ['class'],
   content: [
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -74,5 +73,3 @@ const config: Config = {
   },
   plugins: [require("tailwindcss-animate")],
 }
-
-export default config;
