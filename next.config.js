69ded6595a1a79cb7f0f913984926d78de919072
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-07-13 20:55:52 +0000

    Mirá esta línea clave:
    kotlin
    Copiar
    Editar
    Module parse failed: Unexpe

--- next.config.js
+++ next.config.js
@@ -10,6 +10,16 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    // Agregado para forzar la resolución del SDK de cliente de Firebase
+    // y evitar que se incluya el SDK de Node.js en el bundle del cliente.
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'firebase/auth': 'firebase/auth/dist/index.mjs',
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
