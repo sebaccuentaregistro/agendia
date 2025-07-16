052b9010b64265560f0f24014442d7a4c521a962
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-07-16 21:43:33 +0000

    tudio-9976617208:~/studio{main}$ npm run build
    > agendia@1.0.1 build
    >

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Evita que 'undici' se incluya en los bundles del cliente.
-    // Esto resuelve el error "Module parse failed" causado por dependencias de Firebase/Genkit.
-    if (!isServer) {
-      config.externals.push('undici');
-    }
-    
-    // Solución para el error de empaquetado de chunks de Firebase en el servidor
-    if (isServer) {
-      config.externals.push({
-        '@firebase/app': 'commonjs @firebase/app',
-        '@firebase/auth': 'commonjs @firebase/auth',
-        '@firebase/firestore': 'commonjs @firebase/firestore',
-      });
-    }
-    
-    return config;
-  },
   images: {
     remotePatterns: [
       {
