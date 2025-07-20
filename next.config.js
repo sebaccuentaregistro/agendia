abba7beee5e0235ec768a7dca23e4e105853a068
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-07-20 21:41:39 +0000

    avanza

--- next.config.js
+++ next.config.js
@@ -10,6 +10,20 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer, dev }) => {
+    if (!isServer && !dev) {
+      const { WorkboxWebpackPlugin } = require('workbox-webpack-plugin');
+      config.plugins.push(
+        new WorkboxWebpackPlugin.InjectManifest({
+          swSrc: './src/lib/sw.js',
+          swDest: '../public/sw.js',
+          // Do not cache images by default
+          exclude: [/\.map$/, /_buildManifest\.js$/, /_ssgManifest\.js$/, /_middlewareManifest\.js$/, /manifest\.json$/, /.+?\.png$/i],
+        })
+      );
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
