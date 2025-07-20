66695071378304fe2577fda2fdc37d9efc38fff3
SebaC cuenta registro <sebaccuentaregistro@gmail.com>
2025-07-20 21:49:20 +0000

    studio-9976617208:~/studio{vamos-terminando}$ npm run build
    > agendia@1

--- next.config.js
+++ next.config.js
@@ -12,9 +12,9 @@ const nextConfig = {
   },
   webpack: (config, { isServer, dev }) => {
     if (!isServer && !dev) {
-      const { WorkboxWebpackPlugin } = require('workbox-webpack-plugin');
+      const { InjectManifest } = require('workbox-webpack-plugin');
       config.plugins.push(
-        new WorkboxWebpackPlugin.InjectManifest({
+        new InjectManifest({
           swSrc: './src/lib/sw.js',
           swDest: '../public/sw.js',
           // Do not cache images by default
